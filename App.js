bc469ca71a2e56ff5fb1e1b1cdb1ef7bcca7cab9
Aastha Sharma <xyz@gmail.com>
2021-10-24 10:07:43 +0530

    master

--- App.js
+++ App.js
@@ -1,9 +1,28 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {createAppContainer} from 'react-navigation';
+import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from './screens/HomeScreen';
+import MeteorScreen from './screens/MeteorScreen';
+import ISSLocationScreen from './screens/ISSLocationScreen';
+import { NavigationContainer } from '@react-navigation/native';
 
-export default class App extends React.Component{
-    render(){
-      return(<HomeScreen/>)
-    }
+const Stack=createStackNavigator();
+
+
+ function App (){
+   return(
+    <NavigationContainer>
+    <Stack.Navigator initialRouteName="Home" screenOptions={{headerShown:false}}>
+        <Stack.Screen name="Home" component={HomeScreen}></Stack.Screen>
+        <Stack.Screen name="Meteor" component={MeteorScreen}></Stack.Screen>
+        <Stack.Screen name="ISS" component={ISSLocationScreen}></Stack.Screen>
+    </Stack.Navigator>
+
+</NavigationContainer>
+   );
+   
+    
 }
+
+export default App;
