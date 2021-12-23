4c3f42e65009513e1f1cb86810665dd81443e3f1
Aastha <xyz@gmail.com>
2021-12-23 18:05:40 +0530

    master

--- screens/ISSLocationScreen.js
+++ screens/ISSLocationScreen.js
@@ -1,12 +1,117 @@
+import axios from "axios";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View,Image ,ImageBackground} from "react-native";
+import MapView,{Marker} from 'react-native-maps'
 
 export default class ISSLocationScreen extends React.Component {
+  constructor(){
+    super();
+    this.state={
+      location:{}
+    }
+  }
+  getIssLocation=()=>{
+   axios
+    .get('https://api.wheretheiss.at/v1/satellites/25544')
+        .then((response)=>{
+          this.setState({
+            location:response.data
+          })
+        })
+       .catch((error)=>{
+         alert(error.message)
+       })
+  }
+  componentDidMount(){
+    this.getIssLocation();
+  }
+
+
   render() {
+   if(Object.keys(this.state.location).length===0){
+     return(
+
+      <Text>Loading.....</Text>
+     )
+   }
+
+
+
+else{
+
     return (
-      <View style={{ alignItems: "center", justifyContent: "center", flex: 1 }}>
-        <Text>HomeScreen</Text>
+      <View style={{ flex: 1 }}>
+         <View style={styles.mapContainer}>
+         <MapView style={{width:'100%',height:'100%'}}
+        region={{
+          latitude: this.state.location.latitude,
+          longitude: this.state.location.longitude,
+          latitudeDelta: 100,
+          longitudeDelta: 100
+        }}
+        
+        >
+          <Marker
+          coordinate={{latitude:this.state.location.latitude,longitude:this.state.location.longitude}}
+          >
+            <Image source={require('../assets/iss_icon.png')} style={{width:30,height:30}}/>
+
+          </Marker>
+
+
+
+
+
+
+        </MapView>
+
+         </View>
+        
+        
+        
+        <View style={styles.infoContainer}>
+          <Text style={styles.infoText}>Longitude:{this.state.location.longitude}</Text>
+          <Text style={styles.infoText}>Latitude:{this.state.location.latitude}</Text>
+          <Text style={styles.infoText}>Altitude:{this.state.location.altitude}</Text>
+          <Text style={styles.infoText}>Velocity:{this.state.location.velocity}</Text>
+
+          
+
+
+
+        </View>
+
+
+
+
+
+
+
+
       </View>
     );
+      }
   }
 }
+
+const styles=StyleSheet.create({
+ infoContainer:{
+  flex: 0.2,
+  backgroundColor: 'white',
+  marginTop: -10,
+  borderTopLeftRadius: 30,
+  borderTopRightRadius: 30,
+  padding: 30
+ },
+ infoText:{
+  fontSize: 15,
+  color: "black",
+  fontWeight: "bold"
+ },
+ mapContainer:{
+   flex:0.7
+ }
+
+
+
+})
--- screens/MeteorScreen.js
+++ screens/MeteorScreen.js
@@ -1,12 +1,59 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
-
+import axios from 'axios'
 export default class MeteorScreen extends React.Component {
+  constructor(){
+    super();
+    this.state={
+     meteors:{}
+    }
+  }
+  getMeteorLocation=()=>{
+   axios
+    .get('https://api.nasa.gov/neo/rest/v1/feed?api_key=3WN5YGhfQFbdEWs7s3QqoSXktb8lTfd5zZBxwdDt')
+        .then((response)=>{
+          this.setState({
+            meteors:response.data.near_earth_objects
+          })
+          
+        })
+       .catch((error)=>{
+         alert(error.message)
+       })
+
+  }
+  componentDidMount(){
+    this.getMeteorLocation();
+  }
   render() {
-    return (
-      <View style={{ alignItems: "center", justifyContent: "center", flex: 1 }}>
-        <Text>Meteor Screen</Text>
-      </View>
-    );
+    if(Object.keys(this.state.meteors).length===0){
+      return(
+ 
+       <Text>Loading.....</Text>
+      )
+    }
+    else{ 
+     var meteor_arr=Object.keys(this.state.meteors).map(meteor_date=>{
+        return this.state.meteors[meteor_date]
+      })
+     
+      var meteors=[].concat.apply([],meteor_arr)
+  
+      meteors.forEach(function (element,index) {
+       
+        let diameter = (element.estimated_diameter.kilometers.estimated_diameter_min + element.estimated_diameter.kilometers.estimated_diameter_max) / 2
+        let threatScore = (diameter / element.close_approach_data[0].miss_distance.kilometers) * 1000000000
+        element.threat_score = threatScore;
+    });
+    
+     
+      return (
+        <View style={{ alignItems: "center", justifyContent: "center", flex: 1 }}>
+          <Text>Meteor Screen</Text>
+        </View>
+      )
+
+    }
+   ;
   }
 }
